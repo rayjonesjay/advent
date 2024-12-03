9cd32e485e6d1d5316af9e6a8d576d88d6c627ce
rayjonesjay <rayjaymuiruri@gmail.com>
2024-12-03 23:12:08 +0300

    day4 advent

---
+++ 2024/day4/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"day4/q1"
+	"fmt"
+)
+
+func main(){
+	fmt.Println("helo")
+	q1.Run()
+}
---
+++ 2024/day4/q1/1.go
@@ -0,0 +1,9 @@
+package q1
+
+import (
+	"fmt"
+)
+
+func Run(){
+	fmt.Println("helo from q1")
+}
