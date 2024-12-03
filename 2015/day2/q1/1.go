d9d5ff4f92eb1b0ab4b5f2ec8c4b9b10a7197088
rayjonesjay <rayjaymuiruri@gmail.com>
2024-12-03 23:04:17 +0300

    question1 advent 2015

---
+++ 2015/day2/q1/1.go
@@ -0,0 +1,50 @@
+package q1
+
+import(
+	"os"
+	"bufio"
+	"strings"
+	"strconv"
+	"fmt"
+	"slices"
+)
+
+func Run(){
+	arg := os.Args[1:]
+	if len(arg) != 1 {
+		fmt.Println("go run . <inputfile.txt>")
+		return
+	}
+	
+	fd := Reader(arg[0])
+	scanner := Scanner(fd)
+	res := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		list := strings.Split(line,"x")
+		l,w,h := list[0],list[1],list[2]
+		ll , _ := strconv.Atoi(l)
+		ww , _ := strconv.Atoi(w)
+		hh , _ := strconv.Atoi(h)
+		res = res + (2*ll*ww) + (2*ww*hh) + (2*hh*ll) + FindSmallArea(ll,ww,hh)
+	}
+	fmt.Println(res)
+}
+
+func Scanner(fd *os.File) *bufio.Scanner {
+	return bufio.NewScanner(fd)
+}
+
+func Reader(filename string) (*os.File) {
+	fd, _ := os.Open(filename)
+	return fd
+}
+
+func FindSmallArea(l,w,h int) int {
+	a := l * w 
+	b := w * h
+	c := h * l
+	res := []int{a,b,c}
+	slices.Sort(res)
+	return res[0]
+}
